c03f6fdd896dbf07b51ea0fb69be5ba6bf98465e
AlexandroSoriano <alejandro.soriano@iesdonana.org>
2024-10-25 17:11:48 +0200

    primera carga

---
+++ Actividades/script.js
@@ -0,0 +1,6 @@
+var num = prompt("dame un número: ");
+alert("ese número es: "+num);
+console.log("que hases aquí?");
+document.write("<h3>Saludos</h3>");
+document.getElementById("div1").innerHTML=(num);
+confirm("Me aprueba la asignatura?");
